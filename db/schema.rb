19306babaa511d5741990b3727a74240c8ddf09f
una7 <2ne1.jeremy@gmail.com>
2019-10-04 20:56:18 +0900

    todo

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_04_013417) do
+ActiveRecord::Schema.define(version: 2019_10_04_094743) do
 
   create_table "todo_items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
     t.bigint "todo_list_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "completed_at"
     t.index ["todo_list_id"], name: "index_todo_items_on_todo_list_id"
   end
 
@@ -27,5 +28,4 @@ ActiveRecord::Schema.define(version: 2019_10_04_013417) do
     t.datetime "updated_at", null: false
   end
 
-  add_foreign_key "todo_items", "todo_lists"
 end
